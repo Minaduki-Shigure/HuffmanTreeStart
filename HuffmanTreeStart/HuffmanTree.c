654027b8589276d017d748bdfb27899d91f25451
吴康正 <wukangzheng991025@foxmail.com>
2018-04-25 18:36:41 +0800

    Debug 1.0

--- HuffmanTreeStart/HuffmanTree.c
+++ HuffmanTreeStart/HuffmanTree.c
@@ -2,31 +2,98 @@
 
 void FileStatistic(FILE *fp)
 {
-	char buff;
-	buff = fgetc(fp);
-	while (buff != EOF)
+	char buffer;
+	buffer = fgetc(fp);
+	while (buffer != EOF)
 	{
-		statistics[(int)buff]++;
-		buff = fgetc(fp);
+		statistics[(int)buffer]++;
+		buffer = fgetc(fp);
 	}
 	return;
 }
 
-HuffmanTNode* NewNode(void)
+void TakeSmallestWeight(HuffmanTree tree, int end, int *s1, int *s2)
 {
-	HuffmanTNode *newnode = (HuffmanTNode*)malloc(sizeof(HuffmanTNode));
-	newnode->data = 0;
-	newnode->weight = 0;
-	newnode->Parent = NULL;
-	newnode->LeftChild = NULL;
-	newnode->RightChild = NULL;
-	return newnode;
+	*s1 = 1;
+	*s2 = 1;
+	int buffer[127] = { 0 };
+	int i = 0;
+	int a = 1;
+	for (i = 1; i <= end; i++)
+		buffer[i] = tree[i].weight;
+	for (i = 1; i <= end; i++)
+	{
+		if (buffer[i] <= buffer[*s1])
+			*s1 = i;
+	}
+	for (i = 1; i <= end; i++)
+	{
+		if (i != *s1&&buffer[i] <= buffer[*s2])
+			*s2 = i;
+	}
+	return;
 }
 
-HuffmanTNode *HuffmanTreeInit(int n)
+HuffmanTree HuffmanTreeInit(void)
 {
-	int i, j, m, s1, s2, start;
-	if (n == 1)
-		exit(-1);
-
+	HuffmanTree tree;
+	//int i, j, m, s1, s2, start;
+	int i = 0, n = 0;
+	for (i = 0; i < 128; i++)
+	{
+		if (statistics[i])
+			n++;
+	}
+	int m = 2 * n - 1;
+	tree = (HuffmanTree)malloc((m + 1) * sizeof(HuffmanTNode));
+	int a = 1;
+	for (i = 0; i < 128; i++)
+	{
+		if (statistics[i])
+		{
+			tree[a].data = i;
+			tree[a].weight = statistics[i];
+			tree[a].Parent = 0;
+			tree[a].LeftChild = 0;
+			tree[a].RightChild = 0;
+			a++;
+		}
+	}
+	for (i = a; i <= m; i++)
+	{
+		tree[i].data = 0;
+		tree[i].weight = 0;
+		tree[i].Parent = 0;
+		tree[i].LeftChild = 0;
+		tree[i].RightChild = 0;
+	}
+	int s1, s2;
+	for (i = n + 1; i <= m; i++)
+	{
+		TakeSmallestWeight(tree, i - 1, &s1, &s2);
+		tree[s1].Parent = i;
+		tree[s2].Parent = i;
+		tree[i].LeftChild = s1;
+		tree[i].RightChild = s2;
+		tree[i].weight = tree[s1].weight + tree[s2].weight;
+	}
+	char *cd;
+	int start;
+	unsigned int c, f;
+	cd = (char*)malloc(n * sizeof(char));
+	cd[n - 1] = '\0';
+	for (i = 1; i <= n; i++)
+	{
+		start = n - 1;
+		for (c = i, f = tree[i].Parent; f != 0; c = f, f = tree[f].Parent)
+		{
+			if (tree[f].LeftChild == c)
+				cd[--start] = '0';
+			else
+				cd[--start] = '1';
+		}
+		strcpy(tree[i].code, &cd[start]);
+	}
+	free(cd);
+	return tree;
 }
--- HuffmanTreeStart/HuffmanTree.h
+++ HuffmanTreeStart/HuffmanTree.h
@@ -2,16 +2,19 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 typedef struct{
 	int weight;
 	char data;
-	HuffmanTNode *Parent;
-	HuffmanTNode *LeftChild;
-	HuffmanTNode *RightChild;
-}HuffmanTNode;
+	int Parent;
+	int *LeftChild;
+	int *RightChild;
+	char code[10];
+}HuffmanTNode, *HuffmanTree;
 
 int statistics[127] = { 0 };
 
 void FileStatistic(FILE *fp);
-HuffmanTNode* NewNode(void);
+void TakeSmallestWeight(HuffmanTree tree, int end, int *s1, int *s2);
+HuffmanTree HuffmanTreeInit(void);

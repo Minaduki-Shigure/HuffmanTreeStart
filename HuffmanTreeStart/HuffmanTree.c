625cc232b56ca409151730cc2b9a01691c263e57
吴康正 <wukangzheng991025@foxmail.com>
2018-04-25 16:59:03 +0800

    Debug 0.2

--- HuffmanTreeStart/HuffmanTree.c
+++ HuffmanTreeStart/HuffmanTree.c
@@ -1,5 +1,23 @@
 #include"HuffmanTree.h"
 
+void FileStatistic(FILE *fp)
+{
+	char buff;
+	buff = fgetc(fp);
+	while (buff != EOF)
+	{
+		statistics[(int)buff]++;
+		buff = fgetc(fp);
+	}
+	return;
+}
+
+HuffmanTNode* NewNode(void)
+{
+	HuffmanTNode *newnode = (HuffmanTNode*)malloc(sizeof(HuffmanTNode));
+	return newnode;
+}
+
 HuffmanTNode *HuffmanTreeInit(int n)
 {
 	int i, j, m, s1, s2, start;
--- HuffmanTreeStart/HuffmanTree.h
+++ HuffmanTreeStart/HuffmanTree.h
@@ -3,14 +3,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef int TElemType;
-
 typedef struct{
 	int weight;
-	TElemType data;
+	char data;
 	HuffmanTNode *Parent;
 	HuffmanTNode *LeftChild;
 	HuffmanTNode *RightChild;
 }HuffmanTNode;
 
 int statistics[127] = { 0 };
+
+void FileStatistic(FILE *fp);
+HuffmanTNode* NewNode(void);
